cba8ecc493664122456510ac21edde6ac7c7779e
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-02-29 17:00:00 +0100

    added and installed body parser

--- messages-api.js
+++ messages-api.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+const jsonParser = bodyParser.json();
+
+app.use(jsonParser);
 
 const port = 3000;
+
+app.post("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () =>
   console.log(
