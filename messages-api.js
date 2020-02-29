ef3cce4b58fdbe3b44d3d02f816bc6e54db47cf3
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-02-29 19:00:20 +0100

    turned the limit logic into middleware

--- messages-api.js
+++ messages-api.js
@@ -4,13 +4,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const jsonParser = bodyParser.json();
 
-app.use(jsonParser);
-
-const port = 3000;
-let count = 0;
-app.post("/messages", (req, res, next) => {
+const limitMiddleware = function(req, res, next) {
   if (!req.body.text && count < 5) {
-    console.log("test text property of body", req.body);
+    console.log("test text property of body", req.body.text);
     count++;
     console.log(count);
     res.status(400).json({ message: "error error Nooooboody, does it better" });
@@ -18,10 +14,28 @@ app.post("/messages", (req, res, next) => {
     res.status(429).json({
       message: "Overloading to many requests Self Destruct Initiated"
     });
-  } else {
-    console.log(req.body.text);
-    res.json({ message: "This is the the message that was sent" });
   }
+};
+
+app.use(jsonParser);
+app.use(limitMiddleware);
+
+const port = 3000;
+let count = 0;
+app.post("/messages", function(req, res, next) {
+  // if (!req.body.text && count < 5) {
+  //   console.log("test text property of body", req.body.text);
+  //   count++;
+  //   console.log(count);
+  //   res.status(400).json({ message: "error error Nooooboody, does it better" });
+  // } else if (!req.body.text && count >= 5) {
+  //   res.status(429).json({
+  //     message: "Overloading to many requests Self Destruct Initiated"
+  //   });
+  // } else {
+  console.log(req.body.text);
+  res.json({ message: "This is the the message that was sent" });
+  // }
 });
 
 app.listen(port, () =>
