7c9e6e06a38e8ce360c10ad49ae941aa776abef6
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-02-29 21:45:54 +0100

    limit and offset work for findall not yet for findcountall

--- movie/router.js
+++ movie/router.js
@@ -14,7 +14,9 @@ router.post("/movie", async (req, res, next) => {
 
 router.get("/movie", async (req, res, next) => {
   try {
-    const movies = await Movie.findAll();
+    const limit = req.query.limit || 25;
+    const offset = req.query.offset || 0;
+    const movies = await Movie.findAll({ limit, offset });
     if (movies) {
       res.json(movies);
     } else {
@@ -55,6 +57,22 @@ router.delete("/movie/:id", async (req, res, next) => {
     const movieId = req.params.id;
     const destroyMovie = await Movie.destroy({ where: { id: movieId } });
     res.send({ destroyMovie });
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/movie/count", async (req, res, next) => {
+  try {
+    const limit = Math.min(req.query.limit || 25, 500);
+    const offset = req.query.offset || 0;
+    const { count, rows } = await Movie.findAndCountAll({
+      where: {
+        title: {
+          [Op.like]: "foo%"
+        }
+      }
+    });
   } catch (err) {
     next(err);
   }
